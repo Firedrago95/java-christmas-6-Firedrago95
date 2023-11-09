e47e13a0b833f4997973734ab21580658d8e0907
Firedrago95 <yonghwa95@naver.com>
2023-11-09 21:17:39 +0900

    feat(Date) : 객체생성 기능 구현

---
+++ src/main/java/christmas/Domain/Date.java
@@ -0,0 +1,20 @@
+package christmas.Domain;
+
+public class Date {
+
+    public static final String  INVALID_RANGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final int FIRST_DAY = 1;
+    public static final int LAST_DAY = 31;
+    private final int visitDay;
+
+    public Date(int visitDay) throws IllegalArgumentException{
+        validateNumberRange(visitDay);
+        this.visitDay = visitDay;
+    }
+
+    private void validateNumberRange(int visitDay) throws IllegalArgumentException{
+        if (visitDay < FIRST_DAY || visitDay > LAST_DAY) {
+            throw new IllegalArgumentException(INVALID_RANGE);
+        }
+    }
+}
