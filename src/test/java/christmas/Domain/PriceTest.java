f81588c8f01daf8a0d7006a65939f5bf373cb706
Firedrago95 <yonghwa95@naver.com>
2023-11-10 16:17:58 +0900

    test(PriceTest) : 총 할인금액 계산 테스트 작성

--- src/test/java/christmas/Domain/PriceTest.java
+++ src/test/java/christmas/Domain/PriceTest.java
@@ -1,8 +1,10 @@
 package christmas.Domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,5 +28,21 @@ public class PriceTest {
 
         assertTrue(validPrice.isEligibleForGift());
         assertFalse(invalidPrice.isEligibleForGift());
+    }
+
+    @Test
+    @DisplayName("총 할인금액 계산 테스트")
+    public void calculateTotalDiscountTest() {
+        Map<String, Integer> appliedDiscount = Map.of(
+            "크리스마스 디데이 할인", 1200,
+            "평일 할인", 4046,
+            "특별 할인", 1000
+        );
+        boolean isEligibleForGift = true;
+
+        Price price = new Price(10000);
+
+        assertThat(price.calculateTotalDiscount(appliedDiscount, isEligibleForGift))
+            .isEqualTo(31246);
     }
 }
