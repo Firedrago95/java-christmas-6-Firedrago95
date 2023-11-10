fed83be25852a32e9e394592e0cd04f115629de0
Firedrago95 <yonghwa95@naver.com>
2023-11-10 12:35:18 +0900

    refactor(InputView) : static 메서드로 변경

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -7,10 +7,10 @@ public class InputView {
 
     public static final String REQUEST_ORDER =
         "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
-    private final String REQUEST_VISIT_DAY =
+    private static final String REQUEST_VISIT_DAY =
         "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
-    public int requestVisitDay() {
+    public static int requestVisitDay() {
         try {
             System.out.println(REQUEST_VISIT_DAY);
             return Integer.parseInt(Console.readLine());
@@ -20,7 +20,7 @@ public class InputView {
         }
     }
 
-    public String requestOrder() {
+    public static String requestOrder() {
         System.out.println(REQUEST_ORDER);
         return Console.readLine();
     }
