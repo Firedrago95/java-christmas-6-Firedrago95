dcfa75c572b8f03a4a3dedb4296d4a616c6c167c
Firedrago95 <yonghwa95@naver.com>
2023-11-10 20:26:23 +0900

    refactor(Controller) : 메서드 분리

--- src/main/java/christmas/Controller/Controller.java
+++ src/main/java/christmas/Controller/Controller.java
@@ -11,29 +11,23 @@ import java.util.Map;
 
 public class Controller {
 
+    Date date;
+    Order order;
+    Price price;
+
     public void run() {
-        Date date = createDate();
-        Order order = createOrder();
+        date = createDate();
+        order = createOrder();
         OutputView.printOrder(order.getOrder());
-        int totalPrice = order.calculateTotalPrice();
-        OutputView.printTotalPrice(totalPrice);
-        Price price = new Price(totalPrice);
-        boolean isEventOn = price.isEventOn();
-        if (!isEventOn) {
-            OutputView.printNoEvent(totalPrice);
+        int totalPrice = getTotalPrice();
+        if (hasEvent(totalPrice)) {
             return;
         }
-        boolean isEligibleForGift = price.isEligibleForGift();
-        OutputView.printGift(isEligibleForGift);
-        int visitDay = date.getVisitDay();
-        Discount discount = new Discount();
-        Map<String, Integer> appliedDiscount = discount.applyDiscount(visitDay,
-            order.countDessertItems(), order.countMainItems());
-        OutputView.printDiscount(appliedDiscount, isEligibleForGift);
-        OutputView.printTotalBenefit(
-            price.calculateTotalBenefit(appliedDiscount, isEligibleForGift));
-        OutputView.printTotalPayment(price.calculateTotalPayment(isEligibleForGift));
-        OutputView.printBadge(price.getBadge());
+        boolean isEligibleForGift = hasGift();
+        Map<String, Integer> appliedDiscount = getDiscount(isEligibleForGift);
+        getTotalBenefit(isEligibleForGift, appliedDiscount);
+        getTotalPayment(isEligibleForGift);
+        getChristmasBadge();
     }
 
     private static Date createDate() {
@@ -54,5 +48,51 @@ public class Controller {
             System.out.println(e.getMessage());
             return createOrder();
         }
+    }
+
+    private boolean hasEvent(int totalPrice) {
+        price = new Price(totalPrice);
+        boolean isEventOn = price.isEventOn();
+        if (!isEventOn) {
+            OutputView.printNoEvent(totalPrice);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean hasGift() {
+        boolean isEligibleForGift = price.isEligibleForGift();
+        OutputView.printGift(isEligibleForGift);
+        return isEligibleForGift;
+    }
+
+    private Map<String, Integer> getDiscount(boolean isEligibleForGift) {
+        int visitDay = date.getVisitDay();
+        Discount discount = new Discount();
+        Map<String, Integer> appliedDiscount = discount.applyDiscount(visitDay,
+            order.countDessertItems(), order.countMainItems());
+        OutputView.printDiscount(appliedDiscount, isEligibleForGift);
+        return appliedDiscount;
+    }
+
+    private void getTotalBenefit(boolean isEligibleForGift, Map<String, Integer> appliedDiscount) {
+        int totalBenefit = price.calculateTotalBenefit(appliedDiscount, isEligibleForGift);
+        OutputView.printTotalBenefit(totalBenefit);
+    }
+
+    private int getTotalPrice() {
+        int totalPrice = order.calculateTotalPrice();
+        OutputView.printTotalPrice(totalPrice);
+        return totalPrice;
+    }
+
+    private void getTotalPayment(boolean isEligibleForGift) {
+        int totalPayment = price.calculateTotalPayment(isEligibleForGift);
+        OutputView.printTotalPayment(totalPayment);
+    }
+
+    private void getChristmasBadge() {
+        String badge = price.getBadge();
+        OutputView.printBadge(badge);
     }
 }
