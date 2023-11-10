58523654ddb3a5f3b44d392c49006a77a8e38436
Firedrago95 <yonghwa95@naver.com>
2023-11-10 14:10:59 +0900

    feat(OutputView) : 증정품 출력 기능 추가

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -1,25 +1,34 @@
 package christmas.View;
 
 import christmas.Domain.Menu;
-import java.text.NumberFormat;
 import java.util.Map;
 
 public class OutputView {
 
-    public void printOrder(Map<Menu, Integer> order) {
+    public static void printOrder(Map<Menu, Integer> order) {
         System.out.println("<주문 메뉴>");
         order.forEach(
             (menu, quantity) -> System.out.println(menu.getName() + " " + quantity + "개"));
         System.out.println();
     }
 
-    public void printTotalPrice(int totalPrice) {
+    public static void printTotalPrice(int totalPrice) {
         System.out.println("<할인 전 총주문 금액>");
         System.out.println(convertFormatted(totalPrice) + "원");
         System.out.println();
     }
 
-    private String convertFormatted(int number) {
+    public static void printGift(boolean isEligibleForGift) {
+        if (!isEligibleForGift) {
+            System.out.println("<증정 메뉴>");
+            printNothing();
+            return;
+        }
+        System.out.println("<증정 메뉴>");
+        System.out.println("샴페인 1개");
+    }
+
+    private static String convertFormatted(int number) {
         return String.format("%,d", number);
     }
 
