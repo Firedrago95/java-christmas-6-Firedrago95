b0550ee216e6bb32db355180cca73b7280cf41b0
Firedrago95 <yonghwa95@naver.com>
2023-11-14 13:57:07 +0900

    fix(OutputView) : 혜택 '없음'이 출력되지 않는 버그 수정

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -12,11 +12,12 @@ public class OutputView {
     public static final String BENEFIT_LIST_MESSAGE = "<혜택 내역>";
     public static final String CHAMPAGNE_AMOUNT_MESSAGE = "증정 이벤트: -25,000원";
     public static final String BENEFIT_AMOUNT_MESSAGE = "<총혜택 금액>";
-    public static final String ZERO = "0원";
+    public static final String ZERO_AMOUNT = "0원";
     public static final String TOTAL_PAYMENT_MESSAGE = "<할인 후 예상 결제 금액>";
     public static final String BADGE_MESSAGE = "<12월 이벤트 배지>";
     public static final String CURRENCY = "원";
     public static final String NOTHING = "없음";
+    public static final int ZERO = 0;
 
     public static void printOrder(Map<Menu, Integer> order) {
         System.out.println(ORDER_LIST_MESSAGE);
@@ -48,7 +49,7 @@ public class OutputView {
         System.out.println(BENEFIT_LIST_MESSAGE);
         printNothing();
         System.out.println(BENEFIT_AMOUNT_MESSAGE);
-        System.out.println(ZERO);
+        System.out.println(ZERO_AMOUNT);
         System.out.println();
         System.out.println(TOTAL_PAYMENT_MESSAGE);
         System.out.println(totalPrice + CURRENCY);
@@ -59,15 +60,29 @@ public class OutputView {
 
     public static void printDiscount(Map<String, Integer> appliedDiscount, boolean isEligibleGift) {
         System.out.println(BENEFIT_LIST_MESSAGE);
+        if (sumTotalDiscount(appliedDiscount) == ZERO && !isEligibleGift) {
+            printNothing();
+            return;
+        }
         for (Map.Entry<String, Integer> discount : appliedDiscount.entrySet()) {
-            if (discount.getValue() != 0) {
+            if (discount.getValue() != ZERO) {
                 System.out.println(
-                    discount.getKey() + ": " + "-" + convertFormatted(discount.getValue()) + CURRENCY);
+                    discount.getKey()+": " + "-" +convertFormatted(discount.getValue()) + CURRENCY);
             }
         }
         printGiftAmount(isEligibleGift);
     }
 
+    private static int sumTotalDiscount(Map<String, Integer> appliedDiscount) {
+        return appliedDiscount.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private static void printGiftAmount(boolean isEligibleGift) {
+        if (isEligibleGift) {
+            System.out.println(CHAMPAGNE_AMOUNT_MESSAGE);
+        }
+        System.out.println();
+    }
     public static void printTotalBenefit(int totalBenefit) {
         System.out.println(BENEFIT_AMOUNT_MESSAGE);
         System.out.println("-" + convertFormatted(totalBenefit) + CURRENCY);
@@ -85,15 +100,8 @@ public class OutputView {
         System.out.println(badge);
     }
 
-    private static void printNothing() {
+        private static void printNothing() {
         System.out.println(NOTHING);
-        System.out.println();
-    }
-
-    private static void printGiftAmount(boolean isEligibleGift) {
-        if (isEligibleGift) {
-            System.out.println(CHAMPAGNE_AMOUNT_MESSAGE);
-        }
         System.out.println();
     }
 
