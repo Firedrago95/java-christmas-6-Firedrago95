625a2ccb3d893da54eb760ff4a8b89386083e81b
Firedrago95 <yonghwa95@naver.com>
2023-11-30 12:23:48 +0900

    feat : 주문 입력 유효성 검사 구현

--- src/main/java/christmas/util/InputValidator.java
+++ src/main/java/christmas/util/InputValidator.java
@@ -5,10 +5,22 @@ import java.util.regex.Pattern;
 public class InputValidator {
 
     private static final Pattern dateRegex = Pattern.compile("\\d+");
+    private static final Pattern orderRegex = Pattern.compile("[가-힣]+-\\d+(,[가-힣]+-\\d+)*");
 
     public static void validateDate(String input) throws IllegalArgumentException {
         validateNullOrEmpty(input);
         validateDateForm(input);
+    }
+
+    public static void validateOrder(String input) throws IllegalArgumentException {
+        validateNullOrEmpty(input);
+        validateOrderForm(input);
+    }
+
+    private static void validateOrderForm(String input) throws IllegalArgumentException {
+        if (!orderRegex.matcher(input).matches()) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_FORM.getMessage());
+        }
     }
 
     private static void validateDateForm(String input) throws IllegalArgumentException {
