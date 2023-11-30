dfc782edcd873cb4a10ac77081f4e7ba83fc44c9
Firedrago95 <yonghwa95@naver.com>
2023-11-30 11:35:12 +0900

    feaf : controller, service 객체생성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,5 +1,7 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+
 public class Application {
     public static void main(String[] args) {
         ChristmasController christmasController = new ChristmasController();
---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,12 @@
+package christmas.controller;
+
+import christmas.model.ChristmasService;
+
+public class ChristmasController {
+
+    private final ChristmasService service = new ChristmasService();
+
+    public void run() {
+
+    }
+}
